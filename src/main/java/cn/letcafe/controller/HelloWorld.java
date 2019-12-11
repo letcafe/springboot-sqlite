815031c63e70c1546c4a7d096e098bbd800a14de
dell <letcafe@outlook.com>
2019-12-11 15:48:38 +0800

    add dependencyManagement replace for dependencies

--- src/main/java/cn/letcafe/controller/HelloWorld.java
+++ src/main/java/cn/letcafe/controller/HelloWorld.java
@@ -6,7 +6,6 @@ import cn.letcafe.model.HelloModel;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
-import java.io.IOException;
 import java.util.List;
 
 @RestController
@@ -20,21 +19,18 @@ public class HelloWorld {
     }
 
     @RequestMapping("/")
-    public String Index() {
+    public String index() {
         return "Hello World";
     }
 
     @RequestMapping("/list")
-    public List<HelloModel> List() {
+    public List<HelloModel> list() {
         return helloService.selectAll();
     }
 
     @RequestMapping(value = "/post", method = RequestMethod.POST)
-    public String Post(
-            @RequestBody ReqBody map
-    ) throws  IOException {
+    public String post(@RequestBody ReqBody map) {
         String reqInput = "输入的姓名是" + map.getName() + ",电子邮件是:" + map.getEmail();
-        System.out.println(reqInput);
         helloService.insert(new HelloModel(map.getName(), map.getEmail()));
         return reqInput;
     }
