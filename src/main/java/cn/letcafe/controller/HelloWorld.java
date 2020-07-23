e3e30a1ad7e82d7b73fc127ae6be0adc8eb55839
letcafe <letcafe@outlook.com>
2020-07-23 23:01:05 +0800

    remove some unnecessary dependency

--- src/main/java/cn/letcafe/controller/HelloWorld.java
+++ src/main/java/cn/letcafe/controller/HelloWorld.java
@@ -7,7 +7,9 @@ import lombok.extern.slf4j.Slf4j;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.*;
 
+import java.util.HashMap;
 import java.util.List;
+import java.util.Map;
 
 @Slf4j(topic = "http")
 @RestController
@@ -43,5 +45,20 @@ public class HelloWorld {
         String reqInput = "输入的姓名是" + map.getName() + ",电子邮件是:" + map.getEmail();
         helloService.insert(new HelloModel(map.getName(), map.getEmail()));
         return reqInput;
+    }
+
+    @RequestMapping("/yida/demo")
+    public Map<String, Object> yidaDemo(
+            @RequestParam(name = "name") String name,
+            @RequestParam(name = "companyName") String companyName,
+            @RequestParam(name = "phone") String phone,
+            @RequestParam(name = "loginId") String loginId
+            ) {
+        logger.info("yidaDemo called");
+        logger.info("[param] name = {}, companyName = {}, phone = {}, loginId = {}",
+                name, companyName, phone, loginId);
+        Map<String, Object> res = new HashMap<>();
+        res.put("success", true);
+        return res;
     }
 }
